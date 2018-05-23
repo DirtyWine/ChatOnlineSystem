f2daf85a3d06716009822ac3bb6a5abfc5644843
DirtyWine <769182173@qq.com>
2018-05-23 21:00:27 +0800

    push empty project

---
+++ src/main/java/com/ncu/qianhu/ChatOnlineSystem/ChatOnlineSystemApplication.java
@@ -0,0 +1,12 @@
+package com.ncu.qianhu.ChatOnlineSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatOnlineSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatOnlineSystemApplication.class, args);
+	}
+}
